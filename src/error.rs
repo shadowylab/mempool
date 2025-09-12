3be2c7d27c7c15321d49d4edbf4dda2980003661
Yuki Kishimoto <yukikishimoto@protonmail.com>
2025-09-12 13:29:04 +0200

    Add WebSocket support
    Signed-off-by: Yuki Kishimoto <yukikishimoto@protonmail.com>

--- src/error.rs
+++ src/error.rs
@@ -11,6 +11,18 @@ pub enum Error {
     Url(ParseError),
     /// Reqwest error
     Reqwest(reqwest::Error),
+    /// Tungstenite error
+    #[cfg(feature = "ws")]
+    Tungstenite(tokio_tungstenite::tungstenite::Error),
+    /// JSON error
+    #[cfg(feature = "ws")]
+    Json(serde_json::Error),
+    /// Can't forward websocket message
+    #[cfg(feature = "ws")]
+    CantForwardMessage,
+    /// Unexpected URL scheme
+    #[cfg(feature = "ws")]
+    UnexpectedScheme,
 }
 
 impl std::error::Error for Error {}
@@ -20,6 +32,14 @@ impl fmt::Display for Error {
         match self {
             Self::Url(e) => write!(f, "{e}"),
             Self::Reqwest(e) => write!(f, "{e}"),
+            #[cfg(feature = "ws")]
+            Self::Tungstenite(e) => write!(f, "{e}"),
+            #[cfg(feature = "ws")]
+            Self::Json(e) => write!(f, "{e}"),
+            #[cfg(feature = "ws")]
+            Self::CantForwardMessage => write!(f, "Can't forward websocket message"),
+            #[cfg(feature = "ws")]
+            Self::UnexpectedScheme => write!(f, "Unexpected URL scheme"),
         }
     }
 }
@@ -33,5 +53,19 @@ impl From<ParseError> for Error {
 impl From<reqwest::Error> for Error {
     fn from(e: reqwest::Error) -> Self {
         Self::Reqwest(e)
+    }
+}
+
+#[cfg(feature = "ws")]
+impl From<tokio_tungstenite::tungstenite::Error> for Error {
+    fn from(e: tokio_tungstenite::tungstenite::Error) -> Self {
+        Self::Tungstenite(e)
+    }
+}
+
+#[cfg(feature = "ws")]
+impl From<serde_json::Error> for Error {
+    fn from(e: serde_json::Error) -> Self {
+        Self::Json(e)
     }
 }
